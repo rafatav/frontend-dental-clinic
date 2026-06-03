b71573319126e723a39aabf0a3f4ed9e5ef8f2e1
Rafael Tavares <tavares.rfl@gmail.com>
2026-06-02 21:13:08 -0300

    feat: create template module with layout component

--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./template/template.module').then(m => m.TemplateModule)
+    component: LayoutComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class TemplateRoutingModule { }
